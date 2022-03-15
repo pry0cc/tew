7701d9fc79f6bc522b2cabc1b225ee49f9c99c40
pry0cc <pry0cc@0x00sec.org>
2022-03-15 18:09:21 +0000

    started adding parsing

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/n0ncetonic/nmapxml"
+	"log"
 	"os"
 )
 
@@ -20,21 +23,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	if dnsx != "" {
-		if _, err := os.Stat(dnsx); err != nil {
-			fmt.Printf("dnsx file does not exist\n")
-		}
-	}
+	ParseNmap(input, dnsx)
+}
+
+func ParseNmap(input string, dnsx string) {
+	var index map[string][]string
+	r, _ := nmapxml.Readfile(input)
 
 	if _, err := os.Stat(input); err != nil {
 		fmt.Printf("File does not exist\n")
 	}
 
-	scanData, _ := nmapxml.Readfile(input)
-	ParseNmap(scanData)
-}
+	if dnsx != "" {
+		if _, err := os.Stat(dnsx); err != nil {
+			fmt.Printf("dnsx file does not exist\n")
+		} else {
+			index = ParseDnsx(dnsx)
+		}
+	}
 
-func ParseNmap(r nmapxml.Run) {
 	hostS := r.Host
 	for _, host := range hostS {
 		ipAddr := host.Address.Addr
@@ -42,9 +49,44 @@ func ParseNmap(r nmapxml.Run) {
 			for _, portData := range *host.Ports.Port {
 				if portData.State.State == "open" {
 					portID := portData.PortID
+
+					for _, ipp := range index {
+						fmt.Println(ipp)
+					}
+
 					fmt.Println(ipAddr + ":" + portID)
 				}
 			}
 		}
 	}
+}
+
+func ParseDnsx(filename string) map[string][]string {
+	/* ParseDnsx parses a DNSX JSON file*/
+	var data = map[string][]string{}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var result map[string]interface{}
+		json.Unmarshal([]byte(scanner.Text()), &result)
+		host := result["host"].(string)
+		aRecords := result["a"].([]interface{})
+		ip := ""
+
+		for _, record := range aRecords {
+			ip = record.(string)
+			data[ip] = append(data[ip], host)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
 }
